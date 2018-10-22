20fa0f3d7c7c24c429cdaeea040ba391f6a6f5b5
Jay Welborn <jesse.welborn@gmail.com>
2018-10-22 21:34:31 +0200

    Create PalindromeLinkedList.py

---
+++ Easy/PalindromeLinkedList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        contents = []
+        while head:
+            contents.append(head.val)
+            head = head.next
+        return contents == contents[::-1]
