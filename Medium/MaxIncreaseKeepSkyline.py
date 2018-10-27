cf48d13996518c1a1f4dd7dd88c2f3be54587f71
Jay Welborn <jesse.welborn@gmail.com>
2018-10-27 21:14:33 +0200

    Update MaxIncreaseKeepSkyline.py
    Modified to improve efficiency

--- Medium/MaxIncreaseKeepSkyline.py
+++ Medium/MaxIncreaseKeepSkyline.py
@@ -4,19 +4,15 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        row_maxes = [0] * len(grid)
-        col_maxes = [0] * len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] > row_maxes[i]:
-                    row_maxes[i] = grid[i][j]
-                
-                if grid[i][j] > col_maxes[j]:
-                    col_maxes[j] = grid[i][j]
+        rmax = []
+        cmax = []
+        for i in grid:
+            rmax.append(max(i))
         
-        ret_sum = 0
+        for i in zip(*grid[::-1]):
+            cmax.append(max(i))
+        ret = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] < row_maxes[i] and grid[i][j] < col_maxes[j]:
-                    ret_sum += min(row_maxes[i], col_maxes[j]) - grid[i][j]
-        return ret_sum
+                ret += min(rmax[i], cmax[j]) - grid[i][j]
+        return ret
