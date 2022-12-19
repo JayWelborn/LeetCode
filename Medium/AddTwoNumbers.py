f922facfb7afac931f32bfd9988e3ade04c20f50
Jay Welborn <jesse.welborn@gmail.com>
2022-12-19 10:28:07 -0500

    Update AddTwoNumbers.py
    Alternate solution that creates return list without converting l1 and l2 to numbers first

--- Medium/AddTwoNumbers.py
+++ Medium/AddTwoNumbers.py
@@ -1,34 +1,45 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def get_val(self, ll):
-        num = 0
-        digits = 0
-        while (ll != None):
-            num += ll.val * (10**digits)
-            digits += 1
-            ll = ll.next
-        return num
+    @staticmethod
+    def value_and_remainder(val1: int = 0, val2: int = 0, remainder = 0):
+        ret = val1 + val2 + remainder
+        remainder = 0
+        if ret >= 10:
+            ret = ret % 10
+            remainder = 1
+        return ret, remainder
     
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        sum = self.get_val(l1) + self.get_val(l2)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        ret_list = ListNode(sum % 10)
-        next_node = ret_list
-        while (sum > 0):
-            sum = sum // 10
-            if (sum == 0):
-                return ret_list
-            next_node.next = ListNode(sum % 10)
-            next_node = next_node.next
-        return ret_list
-            
+        val = (l1.val + l2.val) % 10
+        remainder = (l1.val + l2.val) // 10
+        head = ListNode(val=val)
+        tail = head
+        l1 = l1.next
+        l2 = l2.next
+
+        while l1 and l2:
+            val, remainder = self.value_and_remainder(val1=l1.val, val2=l2.val, remainder=remainder)
+            new_node = ListNode(val=val, next=None)
+            tail.next = new_node
+            tail = new_node
+            l1 = l1.next
+            l2 = l2.next
+
+        remaining = l1 or l2
+        
+        while remaining:
+            val, remainder = self.value_and_remainder(val1=remaining.val, remainder=remainder)
+            new_node = ListNode(val=val, next=None)
+            tail.next = new_node
+            tail = new_node
+            remaining = remaining.next
+
+        if remainder:
+            tail.next = ListNode(val=remainder, next=None)
+        
+        return head
