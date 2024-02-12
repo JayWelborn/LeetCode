9630bf0747a847d195423e72019b1dcf229bd595
Jay Welborn <jesse.welborn@gmail.com>
2024-02-12 10:13:16 -0500

    Update MergeSortedArray.py

--- Easy/MergeSortedArray.py
+++ Easy/MergeSortedArray.py
@@ -1,23 +1,26 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i_output = m + n - 1
+        # we must iterate in reverse, to avoid overwriting items we need from our array
+        write_index = m + n - 1
         m -= 1
-        n -=1
+        n -= 1
 
-        while i_output >= 0:
+        while write_index >= 0:
             if m < 0:
-                nums1[i_output] = nums2[n]
+                nums1[write_index] = nums2[n]
                 n -= 1
             elif n < 0:
-                nums1[i_output] = nums1[m]
+                nums1[write_index] = nums1[m]
                 m -= 1
             elif nums1[m] > nums2[n]:
-                nums1[i_output] = nums1[m]
+                nums1[write_index] = nums1[m]
                 m -= 1
             else:
-                nums1[i_output] = nums2[n]
+                nums1[write_index] = nums2[n]
                 n -= 1
-            i_output -= 1
+            write_index -= 1
+
+        return nums1
